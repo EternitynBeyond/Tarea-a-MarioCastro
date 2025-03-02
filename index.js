988eee79f3d95d252bce9ed30bf53ab5adee3855
MarioC <marioecp1994@gmail.com>
2025-03-01 20:21:30 -0500

    Update

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import morgan from "morgan";
 import usersRouter from "./routers/users/users-router.js";
-import inventoryRouter from "./routers/inventory/inventory-router.js";
 import { Database } from "./database/db.js";
+import { rateLimitMiddleware } from "./middlewares/rateLimit.js";
 
 const app = express();
 
@@ -14,9 +14,9 @@ database.setup();
 app.use(cors());
 app.use(morgan());
 app.use(bodyParser());
+app.use(rateLimitMiddleware);
 
 app.use("/users", usersRouter);
-app.use("/inventory", inventoryRouter);
 
 app.listen(8000, () => {
     console.log("App running on port 8000");
--- routers/users/users-router.js
+++ routers/users/users-router.js
@@ -4,28 +4,48 @@ import {
     CreateUsers,
     DeleteUser,
     GetAllUsers,
+    Login,
     UpdateUser,
 } from "./users-controllers.js";
 import { body, param } from "express-validator";
 import validate from "../../middlewares/validate.js";
+import { authMiddleware } from "../../middlewares/auth.js";
 
 const usersRouter = Router();
 
-usersRouter.get("/", GetAllUsers);
+usersRouter.get("/", [authMiddleware], GetAllUsers);
 
 usersRouter.post(
     "/",
-    [body("name").exists().isString(), validate],
+    [
+        body("name").exists().isString().isAlphanumeric(),
+        body("email").exists().isString().isEmail(),
+        body("password").exists().isString().isLength({ min: 4 }),
+        validate,
+    ],
     CreateUsers
+);
+
+usersRouter.post(
+    "/login",
+    [
+        body("email").exists().isString().isEmail(),
+        body("password").exists().isString().isLength({ min: 4 }),
+        validate,
+    ],
+    Login
 );
 
 //  [Patch] localhost:8000/users/2
 usersRouter.patch(
     "/:id",
     [
+        authMiddleware,
         param("id").exists().isNumeric(),
         body("id").not().exists(),
-        body("name").exists().isString(),
+        body("name").optional().isString().isAlphanumeric(),
+        body("email").optional().isString().isEmail(),
+        body("password").optional().isString().isLength({ min: 4 }),
         validate,
     ],
     UpdateUser
@@ -33,7 +53,7 @@ usersRouter.patch(
 //  [DELETE] localhost:8000/users/2
 usersRouter.delete(
     "/:id",
-    [param("id").exists().isNumeric(), validate],
+    [authMiddleware, param("id").exists().isNumeric(), validate],
     DeleteUser
 );
 
